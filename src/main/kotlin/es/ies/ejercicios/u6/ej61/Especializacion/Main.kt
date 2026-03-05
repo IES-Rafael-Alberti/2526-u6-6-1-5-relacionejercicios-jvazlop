99863731e5b81d084bade4535ba3d4fc5dddfbd6
jvazlop <jvazquezl@iesrafaelalberti.es>
2026-03-05 15:01:01 +0100

    Update Main.kt

--- src/main/kotlin/es/ies/ejercicios/u6/ej61/Especializacion/Main.kt
+++ src/main/kotlin/es/ies/ejercicios/u6/ej61/Especializacion/Main.kt
@@ -2,12 +2,12 @@ package es.ies.ejercicios.u6.ej61.Especializacion
 
 fun main() {
 
-    val animal1: Animal = Perro("Rocky")
-    val animal2: Animal = Gato("Misu")
+    val animal1: Animal = Perro("Perro")
+    val animal2: Animal = Gato("Gato")
 
     animal1.hacerSonido()
     animal1.moverse()
 
     animal2.hacerSonido()
     animal2.moverse()
-}
+<}>
